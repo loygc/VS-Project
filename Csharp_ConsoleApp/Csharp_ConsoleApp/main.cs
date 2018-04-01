a296b49241dc3aab6aa41c2766503db54ac25786
susmote <32695256+susmote@users.noreply.github.com>
2018-04-01 08:26:15 +0800

    update

---
+++ Csharp_ConsoleApp/Csharp_ConsoleApp/main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_ConsoleApp
+{
+    class main
+    {
+        static void Main()
+        {
+            Console.WriteLine("hello world");
+            Console.Read();
+        }
+    }
+}
